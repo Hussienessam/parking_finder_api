22ca2ad80f7fff48ea2e976fb078c183629b475b
OmniaFares <omniafares133@gmail.com>
2022-03-29 22:59:42 +0200

    send request using url instead of file

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
+import urllib.request
+from io import BytesIO
+from PIL import Image
 from flask import Flask, request
-
 import os
+
 import main
 
 app = Flask(__name__)
@@ -14,10 +17,25 @@ def find():
         os.makedirs(dir_path)
     
     if request.method == 'POST':
-        static_file = request.files['file']
-        static_file.save(input_path)
-
+        url = request.form['url']
         capacity = request.form['capacity']
+
+        formats = {
+            'image/jpeg': 'JPEG',
+            'image/png': 'PNG',
+            'image/gif': 'GIF',
+            'image/jpg': 'JPG',
+        }
+        response = urllib.request.urlopen(url)
+        image_type = response.info().get('Content-Type')
+        try:
+            format = formats[image_type]
+        except KeyError:
+            raise ValueError('Not a supported image format')
+        file = BytesIO(response.read())
+        img = Image.open(file)
+        img.save(input_path, format=format)
+
         spots = int(capacity) - main.model(input_path)
 
         return str(spots)
