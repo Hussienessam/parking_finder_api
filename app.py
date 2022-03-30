037670af2afdda339d2238050d3795138c034d01
Hussien <hussienessam18@gmail.com>
2022-03-30 07:55:29 +0200

    adjustments to returned response

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from urllib.error import HTTPError
 import urllib.request
 from io import BytesIO
 from PIL import Image
@@ -7,7 +8,7 @@ import os
 import main
 
 app = Flask(__name__)
-@app.route('/find', methods=['GET', 'POST'])
+@app.route('/find', methods=['POST'])
 
 def find():
     input_path = 'local/image.png'
@@ -16,26 +17,48 @@ def find():
     if not os.path.exists(dir_path):
         os.makedirs(dir_path)
     
-    if request.method == 'POST':
-        url = request.form['url']
-        capacity = request.form['capacity']
-
-        formats = {
-            'image/jpeg': 'JPEG',
-            'image/png': 'PNG',
-            'image/gif': 'GIF',
-            'image/jpg': 'JPG',
-        }
-        response = urllib.request.urlopen(url)
-        image_type = response.info().get('Content-Type')
-        try:
-            format = formats[image_type]
-        except KeyError:
-            raise ValueError('Not a supported image format')
-        file = BytesIO(response.read())
-        img = Image.open(file)
-        img.save(input_path, format=format)
-
+    url = request.form['url']
+    capacity = request.form['capacity']
+    
+    imageSaved, error = saveImage(input_path, url)
+    if(imageSaved):
         spots = int(capacity) - main.model(input_path)
-
         return str(spots)
+
+    else:
+        return error
+
+def saveImage(input_path, url):
+    formats = {
+        'image/jpeg': 'JPEG',
+        'image/png': 'PNG',
+        'image/gif': 'GIF',
+        'image/jpg': 'JPG',
+    }
+
+    imageSaved = True
+    error = ''
+
+    try:
+        response = urllib.request.urlopen(url)
+
+    except HTTPError as err:
+        imageSaved = False
+        error = 'File not found'
+        return imageSaved, error
+
+    image_type = response.info().get('Content-Type')
+
+
+    try:
+        format = formats[image_type]
+
+    except KeyError:
+        imageSaved  = False
+        error = 'Not a supported image format'
+        return imageSaved, error
+
+    file = BytesIO(response.read())
+    img = Image.open(file)
+    img.save(input_path, format=format)
+    return imageSaved, error
