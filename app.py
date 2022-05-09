49abfae7e8dd55680ebc5383aa975b918fceef06
Hussien <hussienessam18@gmail.com>
2022-05-09 17:15:50 +0200

    Restructure the project

--- app.py
+++ app.py
@@ -1,70 +1,8 @@
-from urllib.error import HTTPError
-import urllib.request
-from io import BytesIO
-from PIL import Image
-from flask import Flask, request, jsonify
-import os
-
-import main
+from flask import Flask
+import model.model_app as model
 
 app = Flask(__name__)
+
 @app.route('/find', methods=['POST'])
-
 def find():
-    input_path = 'local/image.png'
-    dir_path = './local/'
-
-    if not os.path.exists(dir_path):
-        os.makedirs(dir_path)
-
-    if(request.is_json):
-        content = request.get_json()
-        url = content['url']
-        capacity = content['capacity']
-    else:
-        url = request.form['url']
-        capacity = request.form['capacity']
-    
-    imageSaved, error = saveImage(input_path, url)
-    if(imageSaved):
-        spots = int(capacity) - main.model(input_path)
-        return jsonify(
-            spots = spots
-        )
-    else:
-        return error
-
-def saveImage(input_path, url):
-    formats = {
-        'image/jpeg': 'JPEG',
-        'image/png': 'PNG',
-        'image/gif': 'GIF',
-        'image/jpg': 'JPG',
-    }
-
-    imageSaved = True
-    error = ''
-
-    try:
-        response = urllib.request.urlopen(url)
-
-    except HTTPError as err:
-        imageSaved = False
-        error = 'File not found'
-        return imageSaved, error
-
-    image_type = response.info().get('Content-Type')
-
-
-    try:
-        format = formats[image_type]
-
-    except KeyError:
-        imageSaved  = False
-        error = 'Not a supported image format'
-        return imageSaved, error
-
-    file = BytesIO(response.read())
-    img = Image.open(file)
-    img.save(input_path, format=format)
-    return imageSaved, error
+    return model.find()
---
+++ model/model_app.py
@@ -0,0 +1,67 @@
+from urllib.error import HTTPError
+import urllib.request
+from io import BytesIO
+from PIL import Image
+from flask import request, jsonify
+import os
+
+import model.Model as Model
+
+def find():
+    input_path = 'local/image.png'
+    dir_path = './local/'
+
+    if not os.path.exists(dir_path):
+        os.makedirs(dir_path)
+
+    if(request.is_json):
+        content = request.get_json()
+        url = content['url']
+        capacity = content['capacity']
+    else:
+        url = request.form['url']
+        capacity = request.form['capacity']
+    
+    imageSaved, error = saveImage(input_path, url)
+    if(imageSaved):
+        spots = int(capacity) - Model.model(input_path)
+        return jsonify(
+            spots = spots
+        )
+    else:
+        return error
+
+def saveImage(input_path, url):
+    formats = {
+        'image/jpeg': 'JPEG',
+        'image/png': 'PNG',
+        'image/gif': 'GIF',
+        'image/jpg': 'JPG',
+    }
+
+    imageSaved = True
+    error = ''
+
+    try:
+        response = urllib.request.urlopen(url)
+
+    except HTTPError as err:
+        imageSaved = False
+        error = 'File not found'
+        return imageSaved, error
+
+    image_type = response.info().get('Content-Type')
+
+
+    try:
+        format = formats[image_type]
+
+    except KeyError:
+        imageSaved  = False
+        error = 'Not a supported image format'
+        return imageSaved, error
+
+    file = BytesIO(response.read())
+    img = Image.open(file)
+    img.save(input_path, format=format)
+    return imageSaved, error
