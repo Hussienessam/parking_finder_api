8a07220b5b779ed266936a0c22feeb31848100f0
Hussien <hussienessam18@gmail.com>
2022-05-09 17:41:35 +0200

    create templates

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask
 import model.model_app as model
+import owner_user.owner_app as owner
+import driver_user.driver_app as driver
+import user.user_app as user
 
 app = Flask(__name__)
 
