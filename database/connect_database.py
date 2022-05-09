a2c0b6cb124853c35605351a2ca8cbc48a6d36aa
Hussien <hussienessam18@gmail.com>
2022-05-09 17:52:15 +0200

    modify connect db

--- database/connect_database.py
+++ database/connect_database.py
@@ -1,5 +1,6 @@
 from firebase_admin import credentials, firestore, initialize_app
 
-cred = credentials.Certificate('parkingfinder-589b5-firebase-adminsdk-qp6g6-a3a631e8a6.json')
-default_app = initialize_app(cred)
-db = firestore.client()
+def connect():
+    cred = credentials.Certificate('parkingfinder-589b5-firebase-adminsdk-qp6g6-a3a631e8a6.json')
+    default_app = initialize_app(cred)
+    db = firestore.client()
