652e27b4cc5b21ce61beed72ad6c380ebb5a9bb1
nadahkhaledd <nadahkhaledd@gmail.com>
2022-05-12 23:51:42 +0200

    bookmark done

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask
 import model.model_app as model
 import owner_user.owner_app as owner
-import driver_user.driver_app as driver
-import driver_user.driver_app as driver
+import driver_user.review as review
+import driver_user.bookmark as bookmark
 import user.user_app as user
 import database.connect_database as db_connection
 
@@ -29,14 +29,28 @@ def update_garage():
 def delete_garage():
     return owner.delete(db)
 
+
 @app.route('/add_review', methods=['POST'])
 def add_review():
-    return driver.create(db)
+    return review.create(db)
 
 @app.route('/get_review', methods=['GET'])
 def get_review():
-    return driver.get(db)
+    return review.get(db)
 
 @app.route('/delete_review', methods=['GET'])
 def delete_review():
-    return driver.delete(db)
+    return review.delete(db)
+
+
+@app.route('/add_bookmark', methods=['POST'])
+def add_bookmark():
+    return bookmark.create(db)
+
+@app.route('/get_bookmark', methods=['GET'])
+def get_bookmark():
+    return bookmark.get(db)
+
+@app.route('/delete_bookmark', methods=['GET'])
+def delete_bookmark():
+    return bookmark.delete(db)
---
+++ driver_user/bookmark.py
@@ -0,0 +1,58 @@
+from flask import request, jsonify
+from google.cloud.firestore import GeoPoint
+
+def create(db):
+    try:
+        bookmark_ref = db.collection('bookmark').document()
+        bookmark = {"id": bookmark_ref.id, "name": request.json['name'], "driverID": request.json['driverID'],
+                    "location": GeoPoint(request.json['location']['lat'], request.json['location']['long'])}
+        bookmark_ref.set(bookmark)
+        return jsonify({"success": True}), 200
+    except Exception as e:
+        return f"An Error Occurred: {e}"
+
+
+def get(db):
+    global key
+    bookmark_ref = db.collection('bookmark')
+    try:
+        bookmark_id = request.args.get('id')
+        if bookmark_id:
+            result = bookmark_ref.document(bookmark_id).get().to_dict()
+            record = {}
+            for key in result:
+                if isinstance(result[key], GeoPoint):
+                    record[key] = str(result[key].latitude) + ',' + str(result[key].longitude)
+                    continue
+                record[key] = result[key]
+
+                record['key'] = result[key]
+            return jsonify(record), 200
+
+        else:
+            b = bookmark_ref.stream()
+            all = []
+            for e in b:
+                record = {}
+                result = e.to_dict()
+                for key in result:
+                    if isinstance(result[key], GeoPoint):
+                        record[key] = str(result[key].latitude) + ',' + str(result[key].longitude)
+                        continue
+                    record[key] = result[key]
+
+                    record['key'] = result[key]
+                all.append(record)
+            return jsonify(all), 200
+    except Exception as e:
+        return f"An Error Occurred: {e}"
+
+
+def delete(db):
+    bookmark_ref = db.collection('bookmark')
+    try:
+        bookmark_id = request.json['id']
+        bookmark_ref.document(bookmark_id).delete()
+        return jsonify({"success": True}), 200
+    except Exception as e:
+        return f"An Error Occurred: {e}"
