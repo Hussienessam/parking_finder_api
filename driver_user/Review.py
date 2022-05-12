8be0720662ff94c1d0a35af4625957efd769d56e
nadahkhaledd <nadahkhaledd@gmail.com>
2022-05-12 22:29:52 +0200

    added date field

--- driver_user/Review.py
+++ driver_user/Review.py
@@ -1,12 +1,18 @@
+from datetime import datetime
+
+
 class Review:
     id: str
     content: str
+    date: str
     cameraID: int
     driverID: str
 
-    def fromJson(self, id: str, content: str, cameraid: int, driverid: str) -> None:
+
+    def fromJson(self, id: str, content: str, date: str, cameraid: int, driverid: str) -> None:
         self.id = id
         self.content = content
+        self.date = date
         self.cameraID = cameraid
         self.driverID = driverid
 
