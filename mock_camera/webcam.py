5d7a0fd99a48e893a2da849b506fc66a88914f2f
Hussien <hussienessam18@gmail.com>
2022-07-06 20:16:41 +0200

    mocking camera is done

--- mock_camera/webcam.py
+++ mock_camera/webcam.py
@@ -11,27 +11,23 @@ def mock(db, bucket,storage):
     capacity = request.json['capacity']
     mock_garage = request.json['mock_garage']
 
-    url = "http://192.168.1.8:8080/shot.jpg"
+    url = "http://192.168.100.5:8080/shot.jpg"
     while True:
         img_resp = requests.get(url)
         img_arr = np.array(bytearray(img_resp.content), dtype=np.uint8)
         img = cv2.imdecode(img_arr, -1)
         img = imutils.resize(img, width=1000, height=1800)
         cv2.imshow("life_stream", img)
-        if cv2.waitKey(1) == ord('s'):
-            cv2.imwrite(filename='snap.jpg', img=img)
-            blob = bucket.blob(camera_id+'.jpg')
-            image_data = 'snap.jpg'
-            blob.upload_from_filename(
-                image_data,
-                content_type='image/jpg'
-            )
-            image_url = storage.child(camera_id + '.jpg').get_url(None)
-            snap = {'cameraID': camera_id, 'capacity': capacity, 'path': image_url}
-            return user_queries.update_snaps(db, snap, mock_garage)
-        if cv2.waitKey(1) == 27:
-            break
+        cv2.imwrite(filename='mock_camera/snap.jpg', img=img)
+        blob = bucket.blob(camera_id+'.jpg')
+        image_data = 'mock_camera/snap.jpg'
+        blob.upload_from_filename(
+            image_data,
+            content_type='image/jpg'
+        )
+        image_url = storage.child(camera_id + '.jpg').get_url(None)
+        snap = {'cameraID': camera_id, 'capacity': capacity, 'path': image_url}
+        cv2.destroyAllWindows()
+        return user_queries.update_snaps(db, snap, mock_garage)
 
-    cv2.destroyAllWindows()
-    return "success"
 
